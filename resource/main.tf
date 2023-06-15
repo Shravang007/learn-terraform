a5004121917f827d377ab58c3c55f823293385cc
Shravang007 <shravangoudgajula@gmail.com>
2023-06-15 20:44:09 +0530

    terraform

--- resource/main.tf
+++ resource/main.tf
@@ -1,14 +1,31 @@
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.example.id
-  instance_type = "t3.micro"
+#resource "aws_instance" "web" {
+#  ami           = data.aws_ami.example.id
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "Test"
+#  }
+#}
+#
+#data "aws_ami" "example" {
+#  owners      = ["752442278108"]
+#  most_recent = true
+#  name_regex  = "Centos-8-DevOps-Practice"
+#
+#}
+
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "Centos-8-DevOps-Practice"  # Update with the desired AMI ID
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "Test"
+    Name = "example-instance"
   }
 }
 
-data "aws_ami" "example" {
-  owners      = ["752442278108"]
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-}
